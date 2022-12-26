0747a894644c0e5eb2e7899db5ece9b35fce689c
akhal3d96 <akhal3d96@users.noreply.github.com>
2022-12-26 20:28:23 +0000

    jekyll build from Action abf11c504e14104d5e6cfd0a07e5286b447d96a0

---
+++ projects/solitaire/precache-manifest.be739b67a971db50d1c20a945bf970a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e3778d1ed7fc24e9a48aa7b1ec2059",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "e6d03b5243947ad81503",
+    "url": "/solitaire/static/css/main.fb011c55.chunk.css"
+  },
+  {
+    "revision": "d4ac7e6536eacc9399ae",
+    "url": "/solitaire/static/js/2.65240307.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/solitaire/static/js/2.65240307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d03b5243947ad81503",
+    "url": "/solitaire/static/js/main.d57115e3.chunk.js"
+  },
+  {
+    "revision": "d92fb22c5670dc2c9862",
+    "url": "/solitaire/static/js/runtime-main.08132bb4.js"
+  }
+]);
---
+++ projects/weather/precache-manifest.ccb553f023a472c87f86dfd4a2a5f0fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecbd09a7c198d583b855599ecba1972",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "6fc9a43ccd10e9ab7ff8",
+    "url": "/weather/static/css/main.f06c1bdf.chunk.css"
+  },
+  {
+    "revision": "4975724788bddf1aaea7",
+    "url": "/weather/static/js/2.8901927e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.8901927e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc9a43ccd10e9ab7ff8",
+    "url": "/weather/static/js/main.2be99d57.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "b797a6242c9e3923414787d5e18a1bd4",
+    "url": "/weather/static/media/clouds.b797a624.svg"
+  },
+  {
+    "revision": "f980fc80c89755e5b32190556b766736",
+    "url": "/weather/static/media/fewClouds.f980fc80.svg"
+  },
+  {
+    "revision": "a9704210b11a86f8e407ec6cbc1cdc2f",
+    "url": "/weather/static/media/fewCloudsNight.a9704210.svg"
+  },
+  {
+    "revision": "efb2ab5fa31dd3d1179db1543aa87a60",
+    "url": "/weather/static/media/gear.efb2ab5f.svg"
+  },
+  {
+    "revision": "1ab10811bb80df657543527eaed07d0b",
+    "url": "/weather/static/media/mist.1ab10811.svg"
+  },
+  {
+    "revision": "4f3d1106330c2bc5b14054cfcf71c128",
+    "url": "/weather/static/media/rain.4f3d1106.svg"
+  },
+  {
+    "revision": "fd453ba5899f74eeb6a4b16cf28f6aee",
+    "url": "/weather/static/media/rainNight.fd453ba5.svg"
+  },
+  {
+    "revision": "2ed277e9024d5f50984887fd77583f64",
+    "url": "/weather/static/media/showerRain.2ed277e9.svg"
+  },
+  {
+    "revision": "68d8c2c9ccac7dad49c4b661736835af",
+    "url": "/weather/static/media/skyClear.68d8c2c9.svg"
+  },
+  {
+    "revision": "669440707c4b854138d03ee2c1484f64",
+    "url": "/weather/static/media/skyClearNight.66944070.svg"
+  },
+  {
+    "revision": "18a9fdd586d437934e87aaa0fc48ba22",
+    "url": "/weather/static/media/snow.18a9fdd5.svg"
+  },
+  {
+    "revision": "13f399bfbadfc8736e80d3d8567f8634",
+    "url": "/weather/static/media/thunderstorm.13f399bf.svg"
+  }
+]);
